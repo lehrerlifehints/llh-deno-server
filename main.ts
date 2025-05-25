609744deb2efd831506957ce5a513783ccc8d300
Linus Warnatz <linus@libewa.xyz>
2025-05-25 08:47:54 +0200

    this should allow browsers to import the module

--- main.ts
+++ main.ts
@@ -1,11 +1,16 @@
+// deno-lint-ignore no-process-global
+if (!Deno && !process) {
+  throw new Error(
+    "This script is intended to be run in a Deno or Node.js environment. Please ensure you are using the correct runtime.",
+  );
+}
+
 import ModelClient from "@azure-rest/ai-inference";
 import { AzureKeyCredential } from "@azure/core-auth";
 import { getResponse } from "./getResponse.ts";
 import { getAIStall } from "./aiRequests.ts";
 
-if (Deno) {
-  const token = Deno.env.get("GITHUB_TOKEN") || "";
-}
+const token = Deno.env.get("GITHUB_TOKEN") || "";
 
 if (token == "") {
   throw new Error("Please set the GITHUB_TOKEN environment variable.");
@@ -28,60 +33,58 @@ export async function main() {
   console.info(result);
 }
 
-if (Deno) {
-  export default {
-    async fetch(request) {
-      const url = new URL(request.url);
-      if (url.pathname == "/app/") {
-        const question = url.searchParams.get("query");
-        if (question == null) {
-          return new Response("Question cannot be null", {
-            status: 400,
-            headers: {
-              "Content-Type": "text/plain; charset=utf-8",
-            },
-          });
-        }
-        const answer = await getResponse(question, model, client);
-        return new Response(answer, {
-          status: 200,
-        });
-      } else if (url.pathname == "/stream/") {
-        const question = url.searchParams.get("query");
-        if (question == null) {
-          return new Response("Question cannot be null", {
-            status: 400,
-            headers: {
-              "Content-Type": "text/plain; charset=utf-8",
-            },
-          });
-        }
-
-        const body = new ReadableStream({
-          async start(controller) {
-            controller.enqueue(await getAIStall(question, model, client));
-            controller.enqueue("\n")
-            const response = await getResponse(question, model, client);
-            controller.enqueue(response);
-            controller.close();
-          },
-        });
-        return new Response(body.pipeThrough(new TextEncoderStream()), {
-          headers: {
-            "Content-Type": "text/plain; charset=utf-8",
-          },
-        });
-      } else {
-        return new Response("Not found", {
-          status: 404,
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
+    if (url.pathname == "/app/") {
+      const question = url.searchParams.get("query");
+      if (question == null) {
+        return new Response("Question cannot be null", {
+          status: 400,
           headers: {
             "Content-Type": "text/plain; charset=utf-8",
           },
         });
       }
-    },
-  } satisfies Deno.ServeDefaultExport;
-}
+      const answer = await getResponse(question, model, client);
+      return new Response(answer, {
+        status: 200,
+      });
+    } else if (url.pathname == "/stream/") {
+      const question = url.searchParams.get("query");
+      if (question == null) {
+        return new Response("Question cannot be null", {
+          status: 400,
+          headers: {
+            "Content-Type": "text/plain; charset=utf-8",
+          },
+        });
+      }
+
+      const body = new ReadableStream({
+        async start(controller) {
+          controller.enqueue(await getAIStall(question, model, client));
+          controller.enqueue("\n");
+          const response = await getResponse(question, model, client);
+          controller.enqueue(response);
+          controller.close();
+        },
+      });
+      return new Response(body.pipeThrough(new TextEncoderStream()), {
+        headers: {
+          "Content-Type": "text/plain; charset=utf-8",
+        },
+      });
+    } else {
+      return new Response("Not found", {
+        status: 404,
+        headers: {
+          "Content-Type": "text/plain; charset=utf-8",
+        },
+      });
+    }
+  },
+} satisfies Deno.ServeDefaultExport;
 
 if (import.meta.main) {
   await main();
